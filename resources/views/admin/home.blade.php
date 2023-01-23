2f3a52429ae1321f57cd5457e893d1ad58780349
Thunderbird93 <antircristian@gmail.com>
2023-01-23 23:13:40 +0100

    home, dashboard e guest

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Questa è la Dashboard!</h1>
+</div>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.guest')
+
+@section('content')
+    <h1>Public Page</h1>
+@endsection

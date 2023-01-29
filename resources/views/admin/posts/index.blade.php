79ca539aa683ab04aebb482725a2104e5f439358
Thunderbird93 <antircristian@gmail.com>
2023-01-29 19:51:45 +0100

    .

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,6 +10,27 @@
 <div class="container">
     <h1>Questa si stampano i Post!</h1>
 
-    @dump($posts);
+    <!-- @dump($posts) -->
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Data</th>
+            <th scope="col">Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->date}}</td>
+                    <td>XXX</td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
 </div>
 @endsection
